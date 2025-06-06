4cac07fc2df9fa8c8f838de193df8fa63bf8791e
Pavan-1826 <ragurupavan04@gmail.com>
2025-06-06 15:44:02 +0530

    Add files via upload

---
+++ trees#java/functionadd.java
@@ -0,0 +1,13 @@
+
+public class funtionadd
+{
+    public static void add(){
+        int a=9;
+        int b=9;
+        int c=a+b;
+        System.out.println(c);
+    }
+	public static void main(String[] args) {
+	    add();
+	}
+}
