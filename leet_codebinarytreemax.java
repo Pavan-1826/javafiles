d41dcc8e87369b142e15849eaade26e848a16d9a
Pavan-1826 <ragurupavan04@gmail.com>
2025-06-06 15:47:54 +0530

    Create leet_codebinarytreemax.java

---
+++ leet_codebinarytreemax.java
@@ -0,0 +1,16 @@
+class Solution {
+    private int maxSum = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return maxSum;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(dfs(node.left), 0);
+        int right = Math.max(dfs(node.right), 0);
+        int currentPathSum = node.val + left + right;
+        maxSum = Math.max(maxSum, currentPathSum);
+        return node.val + Math.max(left, right);
+    }
+}
