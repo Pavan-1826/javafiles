9bf532b929c74935ec665054ff8e35a000d2c5e8
Pavan-1826 <ragurupavan04@gmail.com>
2025-06-10 14:33:51 +0530

    Create bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int []arr=new int[5];
+		Scanner sc=new Scanner(System.in);
+		for(int i=0;i<5;i++){
+		    arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<arr.length;i++){
+		    for(int j=0;j<arr.length-1;j++){
+		        if(arr[j]>arr[j+1]){
+		            int temp=arr[j];
+		            arr[j]=arr[j+1];
+		            arr[j+1]=temp;
+		        }
+		    }
+		}
+		for(int i=0;i<5;i++){
+		    	System.out.println(arr[i]);
+		}
+	
+	}
+}
